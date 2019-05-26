888f2a58eee2051dea600e829c9bd07d2fafd674
icebeam030 <icebeam030@outlook.com>
2019-05-26 16:28:20 +1000

    Now basic API to API recommendation works

--- src/main.py
+++ src/main.py
@@ -26,24 +26,46 @@ def text_filter_function(text):
   for token in tokens:
     if token not in text_filter:
       result += porter.stem(token.lower())
+      result += " "
   return result
 
 # process the description column to create new bag of words column
 apis_df["description_words"] = apis_df["description"].apply(text_filter_function)
-mashups_df["description_words"] = mashups_df["description"].apply(text_filter_function)
+# mashups_df["description_words"] = mashups_df["description"].apply(text_filter_function)
 
 # identify importance of words using the tf-idf scheme
-tf_idf = TfidfVectorizer()
+tf_idf = TfidfVectorizer(analyzer = str.split)
 api_description_matrix = tf_idf.fit_transform(apis_df["description_words"])
-mashup_description_matrix = tf_idf.fit_transform(apis_df["description_words"])
+# mashup_description_matrix = tf_idf.fit_transform(mashups_df["description_words"])
 
-# cosine similarity between api and mashup matrix
-cosine_similarity_matrix = cosine_similarity(mashup_description_matrix, api_description_matrix)
-
-# recommend the top k APIs to a mashup
-def recommend_apis(mashup_id, k):
+# recommend the top k related APIs to a given API
+def recommend_apis(api_id, k = 10):
   recommendations = []
-  top_k_recommendations = pd.Series(cosine_similarity_matrix[mashup_id]).sort_values(ascending = False).iloc[0:k]
-  for index in top_k_recommendations.index:
-    recommendations.append(apis_df.iloc[index]["api"])
+  top_k_recommendations = []
+
+  # for all API entries
+  for i in range(api_description_matrix.shape[0]):
+    # if it's not the same as selected API
+    if i != api_id:
+      # calculate the cosine similarity between all other APIs and selected API
+      cos_sim_score_i = cosine_similarity(api_description_matrix[api_id], api_description_matrix[i])[0][0]
+      # append index and related cosine similarity score
+      top_k_recommendations.append((i, cos_sim_score_i))
+
+  # sort the array descending using cosine similarity score, and pick the first k elements
+  top_k_recommendations = sorted(top_k_recommendations, reverse = True, key = lambda x: x[1])[:k]
+
+  for i in top_k_recommendations:
+    # retrieve API's name from apis_df
+    api_name = apis_df.iloc[i[0]]["api"]
+    cos_sim_score = i[1]
+    # append a tuple of API name and its cosine similarity score
+    recommendations.append((api_name, cos_sim_score))
+
+  api_name = apis_df.iloc[api_id]["api"]
+  print("The top " + str(k) + " recommended APIs and their cosine similarity score for " + api_name + " is:\n")
+
   return recommendations
+
+# test recommendation
+recommend_apis(3)
