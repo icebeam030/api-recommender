aaf82c3f3f60129704cc10a610737ad6a9973c97
icebeam030 <icebeam030@outlook.com>
2019-07-17 21:45:41 +1000

    Algorithm is now faster and less buggy

--- src/app.py
+++ src/app.py
@@ -1,28 +1,36 @@
-# the algorithm
+# the algorithm part
 import numpy as np
 import pandas as pd
+
+import nltk
+# download packages in case needed
+nltk.download('stopwords')
+nltk.download('punkt')
+from nltk.corpus import stopwords
+
 from nltk import word_tokenize
 from nltk import PorterStemmer
-from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-class APIRecommender(object):
+class APIRecommender:
+
   def __init__(self):
     # create dataframes from csv
-    self.apis_df = pd.read_csv('../datasets/apis_processed.csv', index_col = 0)
-    self.mashups_df = pd.read_csv('../datasets/mashups_processed.csv', index_col = 0)
+    self.apis_df = pd.read_csv('../datasets/apis_processed.csv')
+    self.mashups_df = pd.read_csv('../datasets/mashups_processed.csv')
 
-    # updated when query is updated, used by recommendation functions
-    self.api_description_matrix = []
+    # updated when query is updated, used by recommendation function
     self.mashup_cos_sim_matrix = []
 
 
   # tokenize and stem text
   def text_filter_function(self, text):
-    # create text filter list of English stopwords and special characters
-    text_filter = list(stopwords.words('english'))
+    # create a text filter list of English stopwords and special characters
+    text_filter = [stopwords.words('english')]
+
     special_characters = [',', '/', '-', '.', ';']
+
     for char in special_characters:
       text_filter.append(char)
 
@@ -36,7 +44,7 @@ class APIRecommender(object):
     tokens = word_tokenize(str(text))
 
     for token in tokens:
-      # remove English stopwords and special characters
+      # remove English stopwords and special characters in each token
       if token not in text_filter:
         # stem each token
         result += porter.stem(token.lower())
@@ -46,10 +54,8 @@ class APIRecommender(object):
 
 
   def add_query(self, query, max_df = 0.85, max_features = 5000):
-    # add a new row for the query to the end of the dataframes
-    apis_df = self.apis_df.append({
-      'description_words': self.text_filter_function(query)
-    }, ignore_index = True)
+    # add a new row for the query to the end of the dataframe
+    # original df is not changed
     mashups_df = self.mashups_df.append({
       'description_words': self.text_filter_function(query)
     }, ignore_index = True)
@@ -59,118 +65,91 @@ class APIRecommender(object):
 
     # apply tf-idf transformation
     mashup_description_matrix = tf_idf.fit_transform(mashups_df['description_words']).toarray()
-    self.api_description_matrix = tf_idf.fit_transform(apis_df['description_words']).toarray()
 
-    # calculate cosine similarity
-    self.mashup_cos_sim_matrix = cosine_similarity(mashup_description_matrix, mashup_description_matrix)
+    # calculate cosine similarity matrix
+    self.mashup_cos_sim_matrix = cosine_similarity(
+      mashup_description_matrix,
+      mashup_description_matrix
+    )
 
-    # return query's id in mashups_df and apis_df
-    query_id_in_mashups = mashups_df.shape[0] - 1
-    query_id_in_apis = apis_df.shape[0] - 1
+    # return query's index in mashups_df to be passed into recommendation function
+    query_index = mashups_df.shape[0] - 1
 
-    return query_id_in_mashups, query_id_in_apis
+    return query_index
 
 
   # return APIs with top scores in the top k related mashups of the query
-  def recommend_apis_from_mashups(self, query_id, k = 15):
-    # sort the top related mashups descending using cosine similarity score, and pick the first k elements
-    score_series = pd.Series(self.mashup_cos_sim_matrix[query_id]).sort_values(ascending = False).iloc[1:k+1]
+  def recommend_apis_from_mashups(self, query_index, k = 15):
+    # sort the top related mashups descending using cosine similarity score
+    # then pick the top k elements, skip the first mashup because it's the query itself
+    score_series = pd.Series(
+      self.mashup_cos_sim_matrix[query_index]
+    ).sort_values(ascending = False).iloc[1 : k + 1]
 
     # get all API id's used in top k related mashups
     apis_in_top_k_mashups = []
 
     for i in range(k):
       # retrieve a list of API id's from mashups_df
-      api_list = self.mashups_df.iloc[score_series.index[i]]['apiList'].split(';')
+      api_list = self.mashups_df.iloc[score_series.index[i]]['api_list'].split(';')
       api_list = [int(api) for api in api_list]
-      # append the list of API id's
+
       apis_in_top_k_mashups.append(api_list)
 
     # filter out repeated APIs from apis_in_top_k_mashups
     all_apis = []
 
-    for i in range(k):
-      for api in apis_in_top_k_mashups[i]:
+    for apis in apis_in_top_k_mashups:
+      for api in apis:
         if api not in all_apis:
           all_apis.append(api)
 
     # for each API, if it is used in one of the top k related mashups, increment its count by 1
     count = {}
 
-    for i in range(len(all_apis)):
-      count[all_apis[i]] = 0
-      for j in range(k):
-        if all_apis[i] in apis_in_top_k_mashups[j]:
-          count[all_apis[i]] += 1
+    for api in all_apis:
+      count[api] = 0
+
+      for apis in apis_in_top_k_mashups:
+        if api in apis:
+          count[api] += 1
 
     # each element in api_scores will be API's id and its count
     api_scores = []
 
-    for i in range(len(all_apis)):
-      api_scores.append([all_apis[i], count[all_apis[i]]])
+    for api in all_apis:
+      api_scores.append({
+        'id': api,
+        'count': count[api]
+      })
 
-    # sort api_scores by count of each API from highest to lowest
+    # sort api_scores by count from highest to lowest
     # only recommend up to 10 APIs
     if (len(api_scores) < 10):
-      api_scores = sorted(api_scores, reverse = True, key = lambda x: x[1])[:len(api_scores)]
+      api_scores = sorted(api_scores, reverse = True, key = lambda x: x['count'])
     else:
-      api_scores = sorted(api_scores, reverse = True, key = lambda x: x[1])[:10]
+      api_scores = sorted(api_scores, reverse = True, key = lambda x: x['count'])[ : 10]
 
     # return a dictionary to be displayed on web page
     recommendations = []
 
-    # for each API in api_scores
-    for i in range(len(api_scores)):
+    # for each element in api_scores
+    for api_score in api_scores:
       # retrieve corresponding row from apis_df
-      api = self.apis_df.loc[self.apis_df['id'] == api_scores[i][0]].values[0]
+      api_row = self.apis_df.iloc[api_score['id'] - 1].values
 
       recommendations.append({
-        'name': api[1],
-        'categories': api[2],
-        'description': api[3],
-        'url': api[4],
-        'count': api_scores[i][1]
+        'name': api_row[1],
+        'categories': api_row[2],
+        'description': api_row[3],
+        'url': api_row[4],
+        'count': api_score['count']
       })
 
     return recommendations
 
 
-  # recommend the top k related APIs to the query
-  def recommend_apis_from_apis(self, query_id):
-    api_scores = []
-
-    # for all API entries except the last one (which is our query)
-    for i in range(query_id):
-      # calculate the cosine similarity between all APIs and the query
-      cos_sim_score_i = cosine_similarity([self.api_description_matrix[query_id]], [self.api_description_matrix[i]])[0][0]
-      # append index and related cosine similarity score
-      api_scores.append([i, cos_sim_score_i])
-
-    # sort the array descending using cosine similarity score, and recommend up to 10 APIs
-    if (len(api_scores) < 10):
-      api_scores = sorted(api_scores, reverse = True, key = lambda x: x[1])[:len(api_scores)]
-    else:
-      api_scores = sorted(api_scores, reverse = True, key = lambda x: x[1])[:10]
-
-    # return a dictionary to be displayed on web page
-    recommendations = []
-
-    # for each API in api_scores
-    for i in range(len(api_scores)):
-      # retrieve corresponding row from apis_df
-      api = self.apis_df.loc[self.apis_df['id'] == api_scores[i][0]].values[0]
-
-      recommendations.append({
-        'name': api[1],
-        'categories': api[2],
-        'description': api[3],
-        'url': api[4]
-      })
-
-    return recommendations
-
-
-# the server
+# the server part
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
@@ -180,22 +159,18 @@ def home():
     max_df = float(request.values.get("max_df"))
     max_features = int(request.values.get("max_features"))
 
+    recommender = APIRecommender()
+
     query = request.values.get("query")
-    query_id_in_mashups, query_id_in_apis = recommender.add_query(query, max_df, max_features)
+    query_index = recommender.add_query(query, max_df, max_features)
 
     if request.values.get("search") == "mashup":
-      apis = recommender.recommend_apis_from_mashups(query_id_in_mashups)
+      apis = recommender.recommend_apis_from_mashups(query_index)
       return render_template("index.html", apis = apis, search = "mashup")
-
-    if request.values.get("search") == "api":
-      apis = recommender.recommend_apis_from_apis(query_id_in_apis)
-      return render_template("index.html", apis = apis, search = "api")
 
   return render_template("index.html")
 
 
 if __name__ == "__main__":
-  print("Initialising API Recommender...")
-  recommender = APIRecommender()
   print("Starting server...")
   app.run(debug = True)
---
+++ src/process_raw_datasets.py
@@ -0,0 +1,65 @@
+# this file cleans up raw csvs and make new csvs ready for use for the server
+# this file is for reference only, it is already executed
+import numpy as np
+import pandas as pd
+
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
+from nltk.corpus import stopwords
+
+from nltk import word_tokenize
+from nltk import PorterStemmer
+
+
+# read csv
+apis_df = pd.read_csv('../datasets/apis.csv', usecols = [0, 1, 2, 3, 5])
+mashups_df = pd.read_csv('../datasets/mashups.csv', usecols = [1, 2, 3, 4, 6])
+
+# rename columns
+mashups_df.columns = ['mashup', 'api_list', 'tag_list', 'description', 'url']
+
+# we are not using tag_list in our calculation
+# so it's ok to keep those rows with null values in this column
+# we fill null values with empty string such that null values won't be dropped later
+tag_list = mashups_df['tag_list']
+tag_list.fillna('', inplace = True)
+
+# drop other null values
+mashups_df.dropna(inplace = True)
+
+# tokenize and stem text
+def text_filter_function(text):
+  # create a text filter list of English stopwords and special characters
+  text_filter = [stopwords.words('english')]
+
+  special_characters = [',', '/', '-', '.', ';']
+
+  for char in special_characters:
+    text_filter.append(char)
+
+  # initialize text stemmer
+  porter = PorterStemmer()
+
+  # string to be returned
+  result = ''
+
+  # tokenize text
+  tokens = word_tokenize(str(text))
+
+  for token in tokens:
+    # remove English stopwords and special characters in each token
+    if token not in text_filter:
+      # stem each token
+      result += porter.stem(token.lower())
+      result += ' '
+
+  return result
+
+# create bag of words column
+apis_df['description_words'] = apis_df['description'].apply(text_filter_function)
+mashups_df['description_words'] = mashups_df['description'].apply(text_filter_function)
+
+# export csv
+apis_df.to_csv('../datasets/apis_processed.csv', index = False)
+mashups_df.to_csv('../datasets/mashups_processed.csv', index = False)
