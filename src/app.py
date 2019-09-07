6de73948d0d900e64edbb3bf935ffa820ac8ce9d
icebeam030 <icebeam030@outlook.com>
2019-09-07 11:53:14 +1000

    Major performance improvements

--- src/app.py
+++ src/app.py
@@ -16,13 +16,14 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 
 class APIRecommender:
+
     def __init__(self):
         # Create dataframes from csv, used to retrieve relevant info
         self.apis_df = pd.read_csv('../datasets/apis_processed.csv')
         self.mashups_df = pd.read_csv('../datasets/mashups_processed.csv')
 
         # Updated when query is updated, used by recommendation function
-        self.mashup_cos_sim_vector = []
+        self.cos_sim_vector = []
 
     def process_text(self, text):
         # Create a text filter list of English stopwords and special characters
@@ -32,17 +33,14 @@ class APIRecommender:
         # Initialize text stemmer
         porter = PorterStemmer()
 
-        processed_text = ''
-
         # Tokenize text
         tokens = word_tokenize(str(text))
-        for token in tokens:
-            # Remove English stopwords and special characters from each token
-            if token not in text_filter:
-                # Stem each token
-                processed_text += porter.stem(token.lower())
-                processed_text += ' '
+        tokens = [
+            porter.stem(token.lower()) for token in tokens
+            if token not in text_filter
+        ]
 
+        processed_text = ' '.join(tokens)
         return processed_text
 
     def add_query(self, query, max_df=0.85, max_features=5000):
@@ -60,18 +58,14 @@ class APIRecommender:
         # Apply tf-idf transformation
         mashup_description_matrix = tf_idf.fit_transform(
             mashups_df['description_words']
-        ).toarray()
+        )
 
         # Calculate cosine similarity vector
-        self.mashup_cos_sim_vector = []
-
         query_index = mashups_df.shape[0] - 1
-        for i in range(query_index):
-            cos_sim_score = cosine_similarity(
-                [mashup_description_matrix[i]],
-                [mashup_description_matrix[query_index]]
-            )
-            self.mashup_cos_sim_vector.append(cos_sim_score)
+        self.cos_sim_vector = cosine_similarity(
+            mashup_description_matrix[query_index],
+            mashup_description_matrix[0 : query_index]
+        )[0]
 
     def recommend_apis_from_mashups(self, k=15):
         """Return APIs with top counts in top k similar mashups to the query"""
@@ -79,24 +73,22 @@ class APIRecommender:
         # Sort the top related mashups descending by cosine similarity score.
         # Then pick the top k elements.
         score_series = pd.Series(
-            self.mashup_cos_sim_vector
+            self.cos_sim_vector
         ).sort_values(ascending=False).iloc[0 : k]
 
         # Get all API id's used in top k related mashups
-        apis_in_top_k_mashups = []
-        for i in range(k):
+        api_set_list = []
+        for i in range(score_series.shape[0]):
             # Retrieve a list of API id's from mashups_df,
             # and convert it to a set
             api_set = set(
                 self.mashups_df.iloc[score_series.index[i]]['api_list'].split(';')
             )
-            api_set = {int(api) for api in api_set}
+            api_set_list.append(api_set)
 
-            apis_in_top_k_mashups.append(api_set)
-
-        # Filter out repeated API id's in apis_in_top_k_mashups
+        # Filter out repeated API id's in api_set_list
         all_apis = set()
-        for api_set in apis_in_top_k_mashups:
+        for api_set in api_set_list:
             all_apis = all_apis | api_set
 
         # For each API, if it is used in one of the top k related mashups,
@@ -104,7 +96,7 @@ class APIRecommender:
         count = {}
         for api in all_apis:
             count[api] = 0
-            for api_set in apis_in_top_k_mashups:
+            for api_set in api_set_list:
                 if api in api_set:
                     count[api] += 1
 
@@ -117,28 +109,22 @@ class APIRecommender:
 
         # Sort api_counts descending by count.
         # Only recommend up to 10 APIs.
-        if (len(api_counts) < 10):
-            api_counts = sorted(
-                api_counts, reverse=True, key=lambda item: item['count']
-            )
-        else:
-            api_counts = sorted(
-                api_counts, reverse=True, key=lambda item: item['count']
-            )[: 10]
+        api_counts.sort(key=lambda item: item['count'], reverse=True)
+        api_counts = api_counts[: 10]
 
         # Return a dictionary to be displayed on web page
         recommendations = []
 
-        for api_count in api_counts:
+        for api in api_counts:
             # Retrieve corresponding row from apis_df
-            api_row = self.apis_df.iloc[api_count['id'] - 1].values
+            api_row = self.apis_df.iloc[int(api['id']) - 1].values
 
             recommendations.append({
                 'name': api_row[1],
                 'categories': api_row[2],
                 'description': api_row[3],
                 'url': api_row[4],
-                'count': api_count['count']
+                'count': api['count']
             })
 
         return recommendations
@@ -148,6 +134,7 @@ class APIRecommender:
 
 from flask import Flask, render_template, request
 app = Flask(__name__)
+recommender = APIRecommender()
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -166,6 +153,5 @@ def home():
 
 
 if __name__ == "__main__":
-    recommender = APIRecommender()
     print("Starting server...")
     app.run(debug=True)
